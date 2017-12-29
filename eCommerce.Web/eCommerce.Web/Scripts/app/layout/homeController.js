2cd1c286dceb1670b26dadbdb30c733776eb8247
Austin Overholt <austinoverholt@gmail.com>
2017-12-29 13:09:51 -0800

    Added angular controller, service and app. Now working on home page.

---
+++ eCommerce.Web/eCommerce.Web/Scripts/app/layout/homeController.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module("app")
+        .controller("homeController", homeController);
+
+    homeController.$inject = ["$scope", "genericService"];
+
+    function homeController($scope, genericService) {
+
+        var vm = this;
+        vm.$scope = $scope;
+        vm.$onInit = _init;
+        vm.genericService = genericService;
+
+
+        function _init() {
+            console.log("Home Ctrl Initialized!");
+        }
+
+
+
+    }
+})();
