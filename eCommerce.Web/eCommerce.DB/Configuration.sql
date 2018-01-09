6e6b45e54e438d9b38e1f0aebb47969968a10231
Austin Overholt <austinoverholt@gmail.com>
2018-01-08 21:54:18 -0800

    Added table to DB

---
+++ eCommerce.Web/eCommerce.DB/Configuration.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Configuration]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ConfigName] NVARCHAR(MAX) NOT NULL, 
+    [ConfigValue] NVARCHAR(MAX) NOT NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+    [DateCreated] DATETIME2 NOT NULL DEFAULT GetUTCDATE(), 
+    [DateModified] DATETIME2 NULL, 
+    [DataType] NVARCHAR(10) NOT NULL, 
+    [Category] NVARCHAR(10) NULL
+)
