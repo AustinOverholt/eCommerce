8ff5e33d2066a65f53ba8b729c820f9d9364a8a9
Austin Overholt <austinoverholt@gmail.com>
2018-01-22 17:02:37 -0800

    Got Api for configuration working

---
+++ eCommerce.Web/eCommerce.Web/Controllers/Api/ConfigurationApiController.cs
@@ -0,0 +1,39 @@
+﻿using eCommerce.Model.Domain;
+using eCommerce.Model.Responses;
+using eCommerce.Services;
+using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace eCommerce.Web.Controllers.Api
+{
+    [RoutePrefix("api/configuration")]
+    public class ConfigurationApiController : ApiController 
+    {
+        private ConfigurationService _configurationService;
+
+        public ConfigurationApiController()
+        {
+            _configurationService = new ConfigurationService();
+        }
+
+        [Route(), HttpGet]
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                ItemsResponse<Configs> resp = new ItemsResponse<Configs>();
+                resp.items = _configurationService.GetConfigString();
+                return Request.CreateResponse(HttpStatusCode.OK, resp);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+
+    }
+}
