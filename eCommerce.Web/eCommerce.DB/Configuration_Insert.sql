3d29b5c6015bc53a1ac302197ffc81b398c2cc08
Austin Overholt <austinoverholt@gmail.com>
2018-01-23 14:59:53 -0800

    Added insert for configs

---
+++ eCommerce.Web/eCommerce.DB/Configuration_Insert.sql
@@ -0,0 +1,50 @@
+﻿CREATE Proc [dbo].[Configuration_Insert]
+	@ConfigName nvarchar(max),
+	@ConfigValue nvarchar(max),
+	@Description nvarchar(max),
+	@DataType nvarchar(10),
+	@Category nvarchar(10),
+	@Id int OUTPUT
+
+/*
+Select * From dbo.Configuration
+
+	Declare  @_id int = 0
+	Declare  @_configName nvarchar(max) = 'test',
+		 @_configValue nvarchar(max) = 'test',
+		 @_description nvarchar(max) = 'test',
+		 @_datatype nvarchar(10) = 'string',
+		 @_category nvarchar(10) = 'connection'
+	Execute dbo.Configuration_Insert
+		 @_configName,
+		 @_configValue,
+		 @_description,
+		 @_datatype,
+		 @_category,
+		 @_id OUTPUT
+
+Select * From dbo.Configuration
+
+
+*/
+
+AS
+BEGIN
+INSERT INTO [dbo].[Configuration]
+           ([ConfigName]
+           ,[ConfigValue]
+           ,[Description]
+           ,[DataType]
+           ,[Category])
+     VALUES
+			(@ConfigName,
+            @ConfigValue,
+			@Description,
+			@DataType,
+			@Category)
+			SET @Id = SCOPE_IDENTITY();
+END
+
+
+
+
